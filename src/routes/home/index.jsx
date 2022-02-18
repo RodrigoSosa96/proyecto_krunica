d6ac4ee8cf7c0a851acf0903bf05bcf1027e8fa6
Rodrigo <rod2008@live.com.ar>
2022-02-18 01:31:43 -0300

    prueba netlify

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -8,16 +8,15 @@ import Contacto from "../../components/contacto";
 import { home } from "../../data.json"
 
 function Home() {
-	console.log(new URL("/src/assets/images/trabajos/trabajo-1.jpg", import.meta.url));
 
 	return (
 		<>
 			<Landing data={home.landing} />
-			{/* <SobreMi data={home.about_me} />
+			<SobreMi data={home.about_me} />
 			<Servicios data=""/>
 			<Clientes data={home.clientes}/>
 			<Prensa />
-			<Contacto /> */}
+			<Contacto />
 		</>
 	);
 
