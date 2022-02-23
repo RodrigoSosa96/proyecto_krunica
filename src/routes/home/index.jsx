ac9302021cbd515df630a44c217658bae42ac727
Rodrigo <rod2008@live.com.ar>
2022-02-23 15:00:41 -0300

    test slider

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -13,11 +13,11 @@ import { home } from "../../data.json"
 function Home() {
 	return (
 		<>
-			<Landing data={home.landing} />
+			{/* <Landing data={home.landing} />
 			<SobreMi data={home.about_me} />
-			<Servicios data="" />
-			{/* <Clientes data={home.clientes} />
-			<Prensa />
+			<Servicios data="" /> */}
+			<Clientes data={home.clientes} />
+			{/* <Prensa />
 			<Contacto /> */}
 		</>
 	);
