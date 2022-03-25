702982c1abb14877a0c7d46ac8508703a08f9da6
Rodrigo <rod2008@live.com.ar>
2022-03-24 21:59:50 -0300

    worker eliminado

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,4 @@
 import { render } from 'preact'
 import App from './app'
-// import Worker from './worker?worker'
-
-const worker = new Worker("/worker.js", { type: 'module' })
 
 render(<App />, document.getElementById('app'))
