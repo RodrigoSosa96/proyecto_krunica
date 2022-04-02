3bd27adcfade992c35c8ac39ebcdb7f525b4323e
Rodrigo Sosa <rod2008@live.com.ar>
2022-04-02 16:20:08 -0300

    Heroku deply testing

--- src/routes/trabajos/index.jsx
+++ src/routes/trabajos/index.jsx
@@ -1,64 +1,8 @@
-import styled from "styled-components";
-
-import { Title } from "../../components/styled/Title.styled";
-
-import { Link, Outlet, useParams } from "react-router-dom";
 import { useEffect, useState } from "preact/hooks";
+import { Link, Outlet, useParams } from "react-router-dom";
+import {Wrapper, TrabajosTitle, Categories } from "./styled";
 
-const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-`
-const Categories = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    gap: 1rem ;
-    width: 90% ;
-    a {
-        text-decoration: none;
-        color: var(--color-text);
-    }
-    @media (min-width: 768px) {
-        gap: 2rem;
-        width: 100% ;
-    }
-    @media(min-width: 992px) {
-        gap: 5rem;
 
-    }
-`
-const TrabajosTitle = styled(Title)`
-    font-weight: 400;
-    font-size: clamp(1rem, 2vw, 2.25rem);
-    user-select: none;
-    :hover {
-        cursor: pointer;
-        font-weight: 500;
-    }
-    :hover::after {
-        opacity: 1;
-        /* transform: scalex(1); */
-    }
-    ${props => props.active && `
-        font-weight: 500;
-        ::after {
-            opacity: 1;
-        }
-    `}
-
-    @media(min-width: 768px) {
-        font-size: 24px;
-    }
-
-    @media(min-width: 992px) {
-        font-size: 26px;
-        line-height: 32px;
-    }
-`
 function Trabajos() {
     const { category } = useParams()
     const [categorie, setCategorie] = useState("taller")
---
+++ src/routes/trabajos/styled.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+import { Title } from "../../components/styled/Title.styled";
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+`
+export const Categories = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 1rem ;
+    width: 90% ;
+    a {
+        text-decoration: none;
+        color: var(--color-text);
+    }
+    @media (min-width: 768px) {
+        gap: 2rem;
+        width: 100% ;
+    }
+    @media(min-width: 992px) {
+        gap: 5rem;
+
+    }
+`
+export const TrabajosTitle = styled(Title)`
+    font-weight: 400;
+    font-size: clamp(1rem, 2vw, 2.25rem);
+    user-select: none;
+    :hover {
+        cursor: pointer;
+        font-weight: 500;
+    }
+    :hover::after {
+        opacity: 1;
+        /* transform: scalex(1); */
+    }
+    ${props => props.active && `
+        font-weight: 500;
+        ::after {
+            opacity: 1;
+        }
+    `}
+
+    @media(min-width: 768px) {
+        font-size: 24px;
+    }
+
+    @media(min-width: 992px) {
+        font-size: 26px;
+        line-height: 32px;
+    }
+`
