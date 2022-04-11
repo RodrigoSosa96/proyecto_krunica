7cbe3a46de80472ba57234c4a49f3c38dfc5f9b6
Rodrigo Sosa <rod2008@live.com.ar>
2022-04-11 18:00:59 -0300

    Arreglos letras en trabajos

--- src/components/trabajos/styled.jsx
+++ src/components/trabajos/styled.jsx
@@ -1,9 +1,46 @@
 import styled from 'styled-components';
 import  M from 'react-masonry-css'
 
+
+export const SectionTitle = styled.h4`
+    font-family: var(--font-title);
+    font-size: 20px;
+    font-weight: 300;
+    line-height: 24.3px;
+    text-align: center;
+    color: var(--color-text);
+
+    border-top: 2px solid #C4C4C4;
+    padding-top: 2.5rem;
+`
+
+export const Masonry = styled(M)`
+    display: flex;
+    margin-left: -1.5rem; // gap
+    /* margin-left: -1vw; // gap */
+    width: 100%;
+    padding-top: 70px;
+
+
+    .my-masonry-grid_column {
+        padding-left: 1.5rem; // gap
+        /* padding-left: 1vw; // gap */
+        background-clip: padding-box;
+
+    }
+    @media(min-width: 992px) {
+        margin-left: -2vw; // gap
+        .my-masonry-grid_column {
+            padding-left: 2vw; // gap
+        }
+    }
+
+    
+`
+
 export const Article = styled.article`
     position: relative;
-    margin-bottom: 2rem; // gap
+    margin-bottom: 1.5rem; // gap
 
     width: 100%;
     //height: 330px;
@@ -23,8 +60,8 @@ export const Article = styled.article`
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        gap: 4rem;
-        gap: clamp(1rem, 1vw, 4rem);
+        gap: min(4rem, 10vw);
+
 
         height: 100%;
         width: 100%;
@@ -34,26 +71,28 @@ export const Article = styled.article`
         font-family: var(--font-menu);
 
         transition: all 0.3s ease-in-out;
-        opacity: 0;
+        opacity: 1;
+        border: 1px solid red;
 
         h4 {
+        border: 1px solid red;
+
         font-size: 1.5rem;
-        //font-size: clamp(1.2rem, 2vw, 1.5rem);
+
         font-weight: 500;
-        width: 60%;
+        width: 70%;
         text-align: center;
         line-height: 1.625rem;
-        //line-height: clamp(1.1rem, 2vw, 1.625rem);
         }
         p {
-        font-size: 1rem;
-        //font-size: clamp(0.8rem, 1vw, 1rem);
-        font-weight: 400;
-        width: 80%;
-        line-height: 1.2rem;
-        //line-height: clamp(1rem, 1vw, 1.2rem);
-        white-space: pre-wrap;
-        padding-bottom: 1rem;
+            border: 1px solid red;
+
+            font-size: 1rem;
+            font-weight: 400;
+            width: 80%;
+            line-height: 1.2rem;
+            white-space: pre-wrap;
+            padding-bottom: 1rem;
         }
     }
     img {
@@ -64,23 +103,45 @@ export const Article = styled.article`
         object-position: center;
         z-index: -1;
     }
+    @media(min-width: 576px) {
+        margin-bottom: 2rem; // gap
+        div {
+            gap: min(3vw);
+            gap: clamp(1rem, 1vw, 4rem);
 
+            h4 {
+                /* font-size: 1.5rem; */
+                font-size: clamp(.9rem, 2.5vw, 1.5rem);
+                /* line-height: 1.5rem; */
+                line-height: clamp(1.1rem, 2.5vw, 1.5rem);
+            }
+            p {
+                font-size: clamp(0.7rem, 2vw, 1rem);
+                line-height: clamp(.8rem, 2vw, 1.2rem);
+                /* font-size: 2rem;
+                line-height: 1.2rem; */
+
+            }
+        }
+    }
+    @media(min-width: 992px) {
+        div {
+            gap: clamp(.5rem, 1vw, 4rem);
+
+            h4 {
+                font-size: clamp(1rem, 1.5vw, 1.5rem);
+                line-height: clamp(1.1rem, 1.5vw, 1.5rem);
+            }
+            p {
+                font-size: clamp(0.7rem, 1vw, 1rem);
+                line-height: clamp(.8rem, 1vw, 1.2rem);
+
+            }
+        }
+    }
 
 `;
-export const Masonry = styled(M)`
-    display: flex;
-    margin-left: -2rem; // gap
-    width: 100%;
-    padding-top: 70px;
 
-
-    .my-masonry-grid_column {
-        padding-left: 2rem; // gap
-        background-clip: padding-box;
-
-    }
-    
-`
 
 export const PhotoModal = styled.div`
     position: fixed;
@@ -149,14 +210,3 @@ export const PhotoModal = styled.div`
     
 `
 
-export const SectionTitle = styled.h4`
-    font-family: var(--font-title);
-    font-size: 20px;
-    font-weight: 300;
-    line-height: 24.3px;
-    text-align: center;
-    color: var(--color-text);
-
-    border-top: 2px solid #C4C4C4;
-    padding-top: 2.5rem;
-`
